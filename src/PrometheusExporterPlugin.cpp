3fcd6f95e68f57eb489463d0294c35157498ef15
smirnov-yv <smirnov-yv@neo-s.ru>
2026-01-20 15:48:06 +0300

    Inital commit

---
+++ src/PrometheusExporterPlugin.cpp
@@ -0,0 +1,65 @@
+#include "PrometheusExporterPlugin.hpp"
+#include <boost/json.hpp>
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+void PrometheusExporter::registerArgs(d3156::Args::Builder &bldr)
+{
+    bldr.addOption(configPath, "PrometheusPath", "path to config for PrometheusExporter.json");
+}
+
+void PrometheusExporter::registerModels(d3156::PluginCore::ModelsStorage &models)
+{
+    MetricsModel::instance() = RegisterModel("MetricsModel", new MetricsModel(), MetricsModel);
+    MetricsModel::instance()->registerUploader(this);
+}
+
+void PrometheusExporter::upload(std::set<Metrics::Metric *> &statistics)
+{
+    metrics_cache = "";
+    for (auto metric : statistics) {
+        std::string data = metric->name + "{";
+        for (int i = 0; i < metric->tags.size(); ++i)
+            data += "," + metric->tags[i].first + "=\"" + metric->tags[i].second + "\"";
+        data += "} " + std::to_string(metric->value_) + "\n";
+        metrics_cache += data;
+    }
+}
+
+void PrometheusExporter::postInit() {
+    if (mode == "pull")
+    {
+        
+        return;
+    }
+    if (mode == "push")
+    {
+        
+        return;
+    }
+    std::cout << R_Prometheus << " unknown Prometheus mode " << mode << "\n";
+}
+
+// ABI required by d3156::PluginCore::Core (dlsym uses exact names)
+extern "C" d3156::PluginCore::IPlugin *create_plugin() { return new PrometheusExporter(); }
+
+extern "C" void destroy_plugin(d3156::PluginCore::IPlugin *p) { delete p; }
+
+namespace json = boost::json;
+
+void PrometheusExporter::parseSettings()
+{
+    if (!std::filesystem::exists(configPath)) {
+        std::cout << R_Prometheus << " Error, file ./status/Page.html not found!!!\n";
+        return;
+    }
+    auto json_text          = (std::ostringstream() << std::ifstream("./status/Page.html").rdbuf()).str();
+    json::value jv          = json::parse(json_text);
+    json::object const &obj = jv.as_object();
+
+    mode             = json::value_to<std::string>(obj.at("mode"));
+    pull_port        = json::value_to<std::uint16_t>(obj.at("pull_port"));
+    push_gateway_url = json::value_to<std::string>(obj.at("push_gateway_url"));
+    job              = json::value_to<std::string>(obj.at("job"));
+}
---
+++ src/PrometheusExporterPlugin.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <PluginCore/IPlugin.hpp>
+#include <PluginCore/IModel.hpp>
+
+#include <MetricsModel.hpp>
+#include <MetricUploader.hpp>
+#include <string>
+
+#define Y_Prometheus "\033[33m[PrometheusExporter]\033[0m "
+#define R_Prometheus "\033[31m[PrometheusExporter]\033[0m "
+#define G_Prometheus "\033[32m[PrometheusExporter]\033[0m "
+#define W_Prometheus "[PrometheusExporter] "
+
+class PrometheusExporter final : public d3156::PluginCore::IPlugin, public Metrics::Uploader
+{
+    std::string configPath       = "./configs/PrometheusExporter.json";
+    std::string mode             = "pull"; // "pull" или "push"
+    std::uint16_t pull_port      = 8000;   // порт по умолчанию
+    std::string push_gateway_url = "http://pushgateway:9091";
+    std::string job              = "MainJob";
+    std::string metrics_cache    = "";
+
+public:
+    void registerArgs(d3156::Args::Builder &bldr) override;
+
+    void registerModels(d3156::PluginCore::ModelsStorage &models) override;
+
+    void postInit() override;
+
+    void upload(std::set<Metrics::Metric *> &statistics) override;
+
+private:
+    void parseSettings();
+};
